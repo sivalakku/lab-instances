b2341ca6ce1aabd55fb8dff58899799abf619426
sivalakku <32267168+sivalakku@users.noreply.github.com>
2018-01-05 22:23:53 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+yum install java jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
